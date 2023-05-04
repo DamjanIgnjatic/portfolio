f5d5803c36f8a7609f07e69880b7e7b416c543d1
DamjanIgnjatic <ignjatic.dev@gmail.com>
2023-05-04 23:10:21 +0200

    Fixing links opening with smooth behavior

--- js/Views/navView.js
+++ js/Views/navView.js
@@ -12,22 +12,26 @@ class NavView {
   smoothScrollHandler() {
     this._allLinks.forEach((link) => {
       link.addEventListener("click", function (e) {
-        e.preventDefault();
         const href = link.getAttribute("href");
 
-        if (href === "#")
+        if (href === "#") {
+          e.preventDefault();
           window.scrollTo({
             top: 0,
             behavior: "smooth",
           });
+        }
 
         if (href !== "#" && href.startsWith("#")) {
+          e.preventDefault();
           const sectionEl = document.querySelector(href);
           sectionEl.scrollIntoView({ behavior: "smooth" });
         }
 
-        if (link.classList.contains("main-nav-link"))
+        if (link.classList.contains("main-nav-link")) {
+          e.preventDefault();
           document.querySelector(".header").classList.toggle("nav-open");
+        }
       });
     });
   }
